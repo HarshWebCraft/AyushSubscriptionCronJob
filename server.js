e7a3315d33fe34f69a0fa6cec1e0be230fc7b546
AyushSantoki1406 <ayushsantoki1462004.com>
2025-07-08 18:32:08 +0530

    ayush cron job

--- server.js
+++ server.js
@@ -378,4 +378,4 @@ app.get("/test-sse/:email", (req, res) => {
   res.json({ message: "Test SSE message sent" });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(8080, () => console.log("Server running on port 5000"));
