a147db26113a8fc9ce7270dbb2d2a79308bc9731
AyushSantoki1406 <ayushsantoki1462004.com>
2025-07-08 19:41:12 +0530

    ayush cron job

--- server.js
+++ server.js
@@ -342,4 +342,4 @@ app.get("/trigger-cron", async (req, res) => {
   }
 });
 
-app.listen(5001, () => console.log("Server running on port 5001"));
+app.listen(8080, () => console.log("Server running on port 5001"));
